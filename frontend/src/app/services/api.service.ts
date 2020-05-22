fac814f5e9794d48b8e49b84750ebb49315ffe7a
annalisape <a.pe@reply.it>
2020-05-22 21:29:58 +0200

    add crus ng methods on service

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Policy } from  '../model/policy';
+import { Observable } from  'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+  PHP_API_SERVER = "http://127.0.0.1:8080";
   constructor(private httpClient: HttpClient) {}
+
+  readPolicies(): Observable<Policy[]> { 
+    return this.httpClient.get<Policy[]>(`${this.PHP_API_SERVER}/api/read.php`);
+  }
+
+  createPolicy( policy: Policy ): Observable<Policy>{
+    return this.httpClient.post<Policy>(`${this.PHP_API_SERVER}/api/create.php`, policy);
+  }
+
+  deletePolicy( id: number ){
+    return this.httpClient.delete<Policy>(`${this.PHP_API_SERVER}/api/delete.php/?id=${id}`);
+  }
 }
