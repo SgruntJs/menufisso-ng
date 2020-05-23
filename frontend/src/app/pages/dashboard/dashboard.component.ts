51f6d74790bbede592b9fef5e6dbccce1eb472a8
annalisape <a.pe@reply.it>
2020-05-23 12:09:20 +0200

    add subscripe on dashboard comp

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../../services/api.service';
+import { Policy } from  '../../model/policy';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  policies:  Policy[];
+  selectedPolicy:  Policy  = { id :  null , number:null, amount:  null};
+  constructor( private api : ApiService) { }
 
   ngOnInit() {
+    this.api.readPolicies().subscribe( ( policies : Policy[] ) => {
+        this.policies = policies;
+        console.log( this.policies );
+    });
   }
 
 }
