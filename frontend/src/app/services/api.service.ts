7f930c5cfcf667a738d3d3458f50f37f2a0876cc
annalisape <a.pe@reply.it>
2020-05-25 07:05:41 +0200

    fix db connection

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -7,21 +7,21 @@ import { Observable } from  'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  PHP_API_SERVER = "http://127.0.0.1:8080";
+  PHP_API_SERVER = "http://localhost/menufisso-ng";
   constructor(private httpClient: HttpClient) {}
 
   readPolicies(): Observable<Policy[]> { 
-    return this.httpClient.get<Policy[]>(`${this.PHP_API_SERVER}/api/read.php`);
+    return this.httpClient.get<Policy[]>(`${this.PHP_API_SERVER}/backend/api/read.php`);
   }
 
   createPolicy( policy: Policy ): Observable<Policy>{
-    return this.httpClient.post<Policy>(`${this.PHP_API_SERVER}/api/create.php`, policy);
+    return this.httpClient.post<Policy>(`${this.PHP_API_SERVER}/backend/api/create.php`, policy);
   }
 
   deletePolicy( id: number ){
-    return this.httpClient.delete<Policy>(`${this.PHP_API_SERVER}/api/delete.php/?id=${id}`);
+    return this.httpClient.delete<Policy>(`${this.PHP_API_SERVER}/backend/api/delete.php/?id=${id}`);
   }
   updatePolicy(policy: Policy){
-    return this.httpClient.put<Policy>(`${this.PHP_API_SERVER}/api/update.php`, policy);   
+    return this.httpClient.put<Policy>(`${this.PHP_API_SERVER}/backend/api/update.php`, policy);   
   }
 }
